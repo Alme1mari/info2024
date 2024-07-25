75abdbe3dbf11e5145f088fbf8a751e1af9a7c59
Alme1mari <mariannafernandadealmeida@gmail.com>
2024-07-25 12:32:10 -0300

    Add files via upload

---
+++ revisao-aula1.js
@@ -0,0 +1,17 @@
+//Marianna Fernanda de Almeida
+//criando variáveis:
+let saudacao= "Olá mundo!"; // Tipo de dado "String"
+let numero= 25; // Tipo de dado inteiro (int)
+let decimal= 3.14; // Tipo de dado flutuante (float)
+let verdadeiro= true; // Tipo de dado booleano (verdadeiro)
+let falso= false; // Tipo de dado booleano (falso)
+
+//Saída dos dados atribu´9idos nas variáveis:
+console.log("")
+console.log("Saudação:", saudacao);
+console.log("********")
+console.log("numero:", numero)
+console.log("decimal:", decimal)
+console.log("verdadeiro:", verdadeiro)
+console.log("falso:", falso)
+console.log("")
